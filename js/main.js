a96c6de0a6b4e1d942700ea2ac8b8349e52977ad
Roman <81470032+shamozavr@users.noreply.github.com>
2022-02-22 23:54:39 +0300

    Нужно больше функций (#3)

---
+++ js/main.js
@@ -0,0 +1,20 @@
+function getNumber(min, max) {
+  if (min > max) {
+    throw new RangeError ('первое значение должно быть минимумом, а второе максимумуом');
+  }
+  if (min === max) {
+    throw new RangeError ('минимум не может быть равен максимуму');
+  }
+  if (min < 0 || max < 0) {
+    throw new RangeError ('min и max должны быть положительными числами');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+getNumber(1, 20);
+
+function checkCommentLength(comment, maxLength) {
+  return (comment.length <= maxLength);
+}
+
+checkCommentLength('123456789', 140);

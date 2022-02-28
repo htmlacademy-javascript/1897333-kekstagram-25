c7d9e83f78650791095a5918c1c99dfc1d35d1c5
Roman Shashin <propeller1992@gmail.com>
2022-02-28 19:47:46 +0300

    дз4

--- js/main.js
+++ js/main.js
@@ -18,3 +18,80 @@ function checkCommentLength(comment, maxLength) {
 }
 
 checkCommentLength('123456789', 140);
+
+const SIMMILAR_OBJECTS_COUNT = 25;
+
+const NAMES = [
+  'Александр',
+  'Дмитрий',
+  'Максим',
+  'Сергей',
+  'Андрей',
+  'Алексей',
+  'Артём',
+  'Илья',
+  'Кирилл',
+  'Михаил',
+  'Никита',
+  'Матвей',
+  'Роман',
+  'Егор',
+  'Арсений',
+  'Иван',
+  'Денис',
+  'Евгений',
+  'Даниил',
+  'Тимофей',
+  'Владислав',
+  'Игорь',
+  'Владимир',
+  'Павел',
+  'Руслан',
+];
+
+const COMMENT = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
+
+const idUnshuffled = [];
+
+for (let i=1 ; i <= SIMMILAR_OBJECTS_COUNT ; i++) {
+  idUnshuffled.push(i);
+}
+
+console.log(idUnshuffled);
+
+/* Randomize array in-place using Durstenfeld shuffle algorithm */
+const shuffleArray = function (array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  return array;
+};
+
+const b = shuffleArray(idUnshuffled);
+console.log(b);
+console.log(b[0]);
+
+const getRandomArrayElement = (elements) => elements[getNumber(0, elements.length - 1)];
+
+const createPost = () => ({
+  id: getRandomArrayElement(b),
+  url: `img/avatar-${  getNumber(1, 6)  }.svg`,
+  description: '',
+  likes: getNumber(15, 200),
+  name: getRandomArrayElement(NAMES),
+  message: getRandomArrayElement(COMMENT)
+});
+
+const similarPosts = Array.from({length: SIMMILAR_OBJECTS_COUNT}, createPost);
+
+console.log(similarPosts);
